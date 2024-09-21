f8c2c971bf755752aaef6613096c4752ad42afb4
Alifkm <alifkahfi@gmail.com>
2024-09-21 21:26:58 +0700

    first commit, adding project to git

---
+++ DRY-Dont't_Repeat_Yourself/MemberPaymentCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DRY_Dont_t_Repeat_Yourself
+{
+    public enum MemberType
+    {
+        Regular,
+        Private,
+        Premium,
+        Gold,
+        Platinum
+    }
+    public class MemberPaymentCalculator
+    {
+        private const int PAYMENT_THRESHOLD = 1000;
+
+        public double CalculateDiscount(MemberType memberType, double money)
+        {
+            double payment = 0;
+
+            switch(memberType)
+            {
+                case MemberType.Regular:
+                    payment = money > PAYMENT_THRESHOLD ? 0.1 : 0.05;
+                    break;
+                case MemberType.Private:
+                    payment = money > PAYMENT_THRESHOLD ? 0.2 : 0.15;
+                    break;
+                case MemberType.Premium:
+                    payment = money > PAYMENT_THRESHOLD ? 0.3 : 0.25;
+                    break;
+                case MemberType.Gold:
+                    payment = money > PAYMENT_THRESHOLD ? 0.4 : 0.35;
+                    break;
+                case MemberType.Platinum:
+                    payment = money > PAYMENT_THRESHOLD ? 0.5 : 0.45;
+                    break;
+            }
+
+            return payment * money;
+        }
+    }
+}
---
+++ DRY-Dont't_Repeat_Yourself/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DRY_Dont_t_Repeat_Yourself
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MemberPaymentCalculator MemberPaymentCalculator = new MemberPaymentCalculator();
+            
+            double regularMemberPayment = MemberPaymentCalculator.CalculateDiscount(MemberType.Regular, 1200);
+            Console.WriteLine(regularMemberPayment);
+
+            double privateMemberPayment = MemberPaymentCalculator.CalculateDiscount(MemberType.Private, 2000);
+            Console.WriteLine(privateMemberPayment);
+
+            double premiumMemberPayment = MemberPaymentCalculator.CalculateDiscount(MemberType.Premium, 800);
+            Console.WriteLine(premiumMemberPayment);
+
+            double goldMemberPayment = MemberPaymentCalculator.CalculateDiscount(MemberType.Gold, 1000);
+            Console.WriteLine(goldMemberPayment);
+
+            double platinumMemberPayment = MemberPaymentCalculator.CalculateDiscount(MemberType.Platinum, 5000);
+            Console.WriteLine(platinumMemberPayment);
+
+        }
+    }
+}
